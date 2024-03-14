0fb82bfc279cafb5d75576f608cbd436223fb1e0
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-03-14 18:21:04 +0000

    Update BarcodeScannerActivity.java

--- src/android/BarcodeScannerActivity.java
+++ src/android/BarcodeScannerActivity.java
@@ -349,7 +349,7 @@ private class ScannerAnalyzer implements ImageAnalysis.Analyzer {
             byte[] bytes = new byte[mediaImage.getPlanes()[0].getBuffer().remaining()];
             mediaImage.getPlanes()[0].getBuffer().get(bytes);
             imageBuffer.put(bytes);
-           InputImage inputImage = InputImage.fromByteBuffer(ByteBuffer.wrap(imageBuffer), mediaImage.getWidth(), mediaImage.getHeight(), 0, InputImage.IMAGE_FORMAT_NV21);
+           InputImage inputImage = InputImage.fromByteBuffer(imageBuffer, mediaImage.getWidth(), mediaImage.getHeight(), 0, InputImage.IMAGE_FORMAT_NV21);
 
             
             // Process image for barcode detection
